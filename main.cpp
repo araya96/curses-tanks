2991adb6b752554c3603408f53e2432a8d3c2516
nkwenner <nwenner@carthage.edu>
2017-05-08 10:53:42 -0500

    fixed the player's struct and put it back into seperate source file

--- main.cpp
+++ main.cpp
@@ -100,19 +100,7 @@ struct Player
 	int hits;
 };
 
-void Player::Initialize()
-{
-	position = hits = 0;
-	angle = 45.0;
-	power = 50.0;
-}
 
-// draws a player on the landscape
-void Player::Draw(vector<int> ground)
-{
-	move(ground.at(position) - 1, position + 1);
-	addch('o');
-}
 
 // sets up display of stats
 void Display(Player * players, int turn)
