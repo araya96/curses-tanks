d6f2172abb2b51969e3b1feb125046bd6b0bd6fd
nkwenner <nwenner@carthage.edu>
2017-05-09 10:20:28 -0500

    fixed up colors slightly

--- main.cpp
+++ main.cpp
@@ -303,7 +303,7 @@ int main()
 	keypad(stdscr, true);
 	start_color();
 	init_pair(1, COLOR_WHITE, COLOR_BLACK);
-	init_pair(2, COLOR_MAGENTA, COLOR_BLACK);
+	init_pair(2, COLOR_RED, COLOR_BLACK);
 	init_pair(3, COLOR_YELLOW, COLOR_BLACK);
 	init_pair(4, COLOR_GREEN, COLOR_BLACK);
 	init_pair(5, COLOR_CYAN, COLOR_BLACK);
@@ -330,7 +330,7 @@ int main()
 		attron(COLOR_PAIR(4));
 		ground.Draw();
 		attron(COLOR_PAIR(3));
-		players[0].Draw(ground, '#');
+		players[0].Draw(ground, 'O');
 		attron(COLOR_PAIR(2));
 		players[1].Draw(ground, 'O');
 		Display(players, turn, wind);
